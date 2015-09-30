52a3aee91d67186857b26196881967201e97587c
Matthieu Herrb <matthieu.herrb@laas.fr>
2015-09-30 11:48:59 +0200

    update copyright notices

--- echoc.c
+++ echoc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 Matthieu Herrb <matthieu@herrb.eu>
+ * Copyright (c) 2012-2015 Matthieu Herrb <matthieu@herrb.eu>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
